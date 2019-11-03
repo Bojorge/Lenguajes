5b6efd1a7fce6b8bcc8221a6ef5a3740b5c78921
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-11-03 01:17:46 -0600

    solo funciona con boing717 y emergencia

--- MayCEy.pl
+++ MayCEy.pl
@@ -1,65 +1,122 @@
 
 inicio:-
-write('identifiquese '),
+espacio,
+write('Hola soy MayCEy'),
+nl,
+write('*  Si necesita ayuda  *  >>>  IDENTIFIQUESE : '),
 read(X),
-identidadAvion(X),
-write('en que le puedo ayudar '),
-read(Y),
-emergencia(Y),
-undo.
+identidadAvion(X).
 
+continuar:-
+espacio,
+write('¿algo mas? >>> s/n'),
+read(X),
+( (X == s)
+->
+ayuda ;
+finalizar).
+
+ayuda:-
+espacio,
+write('¿que necesita?'),
+read(X),
+X.
+
+finalizar:-
+espacio,
+write('fue un gusto ayudarle, adios. '),
+espacio,
+write(' * * *  ...  * * *  ...  * * * '),
+inicio.
 
 /*identidad del avion para asignarle una pista*/
 identidadAvion(beechcraft) :-  beechcraft, !.
 identidadAvion(boing717) :- boing717, !.
 identidadAvion(a340) :-  a340, !.
+identidadAvion(emergencia) :- emergencia, !.
 
 
 
 beechcraft:-
+espacio,
 write('le corresponde la pista 1'),
 nl,
 p1.
 
 boing717:-
-write('le corresponde la pista 2 '),
+espacio,
+write('le corresponde una de las pistas 2A o 2B '),
 nl,
-p2.
+nl,
+write('Necesita  ???  >>>  aterrizar/despegar : '),
+read(X),
+X,
+p2_1.
 
 a340:-
+espacio,
 write('le corresponde la pista 3'),
 nl,
 p3.
 
 
 p1:-
-verificar(aterrizar),
-verificar(despegar).
+espacio,
+write('Tiene disponible un kilometro de pista '),
+nl.
 
 
-p2:-
-verificar(aterrizar),
-verificar(despegar).
+p2_1:-
+espacio,
+write('Tiene disponibles 2 kilometros de pista '),
+nl,
+write('Esta pista tiene una direccion de Este a Oeste'),
+continuar.
+
+p2_2:-
+espacio,
+write('Tiene disponible 2 kilometros de pista '),
+nl,
+write('Esta pista tiene una direccion de Oeste a Este').
+
 
 
 p3:-
-verificar(aterrizar),
-verificar(despegar).
+espacio,
+write('Tiene disponibles 3 kilometros de pista ').
 
-emergencia(secuestro):- secuestro.
-emergencia(nocombustible):- nocombustible.
+
+emergencia :-
+espacio,
+write('¿Cual es su emergencia? '),
+read(X),
+X.
+
 
 
 secuestro:-
+espacio,
 write('llamare a la policia para que se encargue del secuestro'),
 nl,
-inicio.
+continuar.
 
 
 nocombustible:-
+espacio,
 write('buscaremos una pista para que aterrice lo mas pronto posible'),
 nl,
-inicio.
+continuar.
+
+
+aterrizar:-
+espacio,
+write('Tiene permiso para aterrizar en la pista indicada al inicio '),
+continuar.
+
+despegar:-
+espacio,
+write('Tiene permiso para despegar en la pista indicada al inicio '),
+continuar.
 
 
 
@@ -92,5 +149,6 @@ undo :- retract(no(_)),fail.
 undo.
 
 
-
+espacio:-
+nl,nl,nl,nl,nl,nl.
 
